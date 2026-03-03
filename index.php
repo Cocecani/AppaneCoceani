d9ffca007bf2e8b0b8d812610f613701211f1bfd
Cocecani <coceanichristopher07@gmail.com>
2026-03-03 22:24:07 +0100

    piccole modifiche a login e implementazione menu settimanale

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 require('db.php');
 include('header.php');
 
@@ -33,8 +34,35 @@ function creaProd($nome, $ingredienti, $quantita, $prezzo)
         <h1>Menù settimanale</h1>
         <div class="menu">
             <?php
-            $sql = "SELECT id, nome, prezzo, quantità FROM tprodotto";
+            $sqlMenu = "SELECT * FROM tmenu";
+            $resultMenu = $conn->query($sqlMenu);
 
+            if ($resultMenu->num_rows > 0) {
+                while ($rowMenu = $resultMenu->fetch_assoc()) {
+                    $stmtProd = $conn->prepare("SELECT nome, prezzo, quantità FROM tprodotto WHERE id = ?");
+                    $stmtProd->bind_param("i", $rowMenu["idprodotto"]);
+                    $stmtProd->execute();
+                    $resultProd = $stmtProd->get_result();
+                    if ($resultProd->num_rows > 0) {
+                        while ($rowProd = $resultProd->fetch_assoc()) {
+                            $stmtIng = $conn->prepare("SELECT ingrediente FROM tricetta WHERE idProdotto = ?");
+                            $stmtIng->bind_param("i", $rowMenu["idprodotto"]);
+                            $stmtIng->execute();
+                            $resultI = $stmtIng->get_result();
+                            $ingredienti = [];
+                            while ($rowI = $resultI->fetch_assoc()) {
+                                $ingredienti[] = $rowI['ingrediente'];
+                            }
+                            echo creaProd($rowProd["nome"], $ingredienti, $rowProd["quantità"], $rowProd["prezzo"]);
+                        }
+                    }
+                }
+            } else {
+                echo '<p> Non ci sono prodotti disponibili per l\'acquisto fino a giovedì</p>';
+            }
+
+/*
+            $sql = "SELECT id, nome, prezzo, quantità FROM tprodotto";
             $result = $conn->query($sql);
 
             if ($result->num_rows > 0) {
@@ -47,12 +75,12 @@ function creaProd($nome, $ingredienti, $quantita, $prezzo)
                     while ($rowI = $resultI->fetch_assoc()) {
                         $ingredienti[] = $rowI['ingrediente'];
                     }
-
                     echo creaProd($row["nome"], $ingredienti, $row["quantità"], $row["prezzo"]);
                 }
             } else {
                 echo "non ci sono prodotti disponibili";
             }
+*/
 
             ?>
         </div>
