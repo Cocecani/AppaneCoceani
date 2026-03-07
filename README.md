cfeddf32125b49e00fdc632808051cffdd941561
Cocecani <coceanichristopher07@gmail.com>
2026-03-07 17:32:25 +0100

    movements

--- README.md
+++
@@ -1,6 +0,0 @@
-
-Elemento	    Colore
-Back ground   : #fffefe
-primo piano	  : #d6923f
-testo	        : #fdf9ce
-testo extra	  : #181818
--- header.php
+++
@@ -1,9 +0,0 @@
-<div class="topbar">
-<h1>Appane</h1>
-<div style="margin: auto 0;">
-    <a href="login.php" class="button" style="margin-right: 10px;"><img src="grafica/img/user.png" alt="user" class="header-icon"></a>
-    <a href="cart.php" class="button"><img src="grafica/img/cart.png" alt="cart" class="header-icon"></a>
-</div>
-
-
-</div>
