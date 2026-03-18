9488a34385901562f64de740379354213c9b46c1
Cocecani <coceanichristopher07@gmail.com>
2026-03-18 13:59:13 +0100

    db

--- includes/db.php
+++ includes/db.php
@@ -5,21 +5,12 @@ $username = "quintaf";
 $password = "Qu!nta";
 $dbname = "appane_coceani";
 
-<<<<<<< HEAD
 /*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "appane_coceani";
 */
-=======
-
-/*$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "appane_coceani";*/
-
->>>>>>> 790c9410aa3c748a0562b6cd4c3bb6193804679a
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
